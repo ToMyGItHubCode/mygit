ba2575e622db7e074ee49f82fbae822c9aa7e031
zhangsuikang <1120309435@qq.com>
2019-11-12 22:16:46 +0800

    test1

---
+++ src/main/java/com/git/demo/MyTest.java
@@ -0,0 +1,13 @@
+package com.git.demo;
+
+/**
+ * @author dell
+ * @create 2019-11-12 21:43
+ */
+public class MyTest {
+
+    public static void main(String[] args) {
+        System.out.println("this is  test");
+        System.out.println("this is update version");
+    }
+}
