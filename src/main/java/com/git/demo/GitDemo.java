136e390129a13bc38a1d016e856658bae275cbdf
zhangsuikang <1120309435@qq.com>
2019-11-12 17:12:38 +0800

    Initial commit

---
+++ src/main/java/com/git/demo/GitDemo.java
@@ -0,0 +1,12 @@
+package com.git.demo;
+
+/**
+ * @author dell
+ * @create 2019-11-12 17:06
+ */
+public class GitDemo {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git!!");
+    }
+}
