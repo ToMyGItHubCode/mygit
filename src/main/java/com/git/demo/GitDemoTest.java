31fb1dc96596339b75d23612cd804b7e90b50b3f
zhangsuikang <1120309435@qq.com>
2019-11-12 17:27:03 +0800

    test

---
+++ src/main/java/com/git/demo/GitDemoTest.java
@@ -0,0 +1,14 @@
+package com.git.demo;
+
+/**
+ * @author dell
+ * @create 2019-11-12 17:14
+ */
+public class GitDemoTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("Git test");
+    }
+
+}
